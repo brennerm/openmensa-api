9e4e98d1c679f30ae8a137c1a4f325a2d19b7808
brennerm <xamrennerb@gmail.com>
2018-02-02 23:56:53 +0100

    implement limit and page handling

--- openmensa.py
+++ openmensa.py
@@ -34,12 +34,31 @@ class OpenMensa:
         )
 
     @staticmethod
-    def canteens(near_lat_lng=None, near_dist=None, canteen_ids=None, has_coords=None, limit=10):
+    def get_all(method, *args, **kwargs):
+        res = []
+        kwargs['page'] = 0
+
+        while True:
+            response = method(*args, **kwargs)
+
+            if not response:
+                break
+
+            res.extend(response)
+            kwargs['page'] += 1
+
+        return res
+
+
+    @staticmethod
+    def canteens(near_lat_lng=None, near_dist=None, canteen_ids=None, has_coords=None, page=None, limit=None):
         """Get a list of canteens
         :param near_lat_lng: position 2 tuple to define search center, example (52.39, 13.12)
         :param near_dist: search distance in km for near_lat_lng, default: 10
         :param canteen_ids: list of canteen ids to query
         :param has_coords: whether to return canteens with or without coordinations
+        :param page: page index to query
+        :param limit: restrict response to this amount of results
         """
         params = {}
 
@@ -55,6 +74,12 @@ class OpenMensa:
         if has_coords is not None:
             params['hasCoordinates'] = has_coords
 
+        if page:
+            params['page'] = page
+
+        if limit:
+            params['limit'] = limit
+
         return OpenMensa.__make_json_request('canteens', params)
 
     @staticmethod
@@ -68,16 +93,24 @@ class OpenMensa:
         )
 
     @staticmethod
-    def canteen_days(canteen_id, start_date=None):
+    def canteen_days(canteen_id, start_date=None, page=None, limit=None):
         """Get all canteen days
 
         :param canteen_id: id of canteen to query
         :param start_date: the date to begin with
+        :param page: page index to query
+        :param limit: restrict response to this amount of results
         """
 
         params = {}
         if start_date:
             params['start'] = start_date
+
+        if page:
+            params['page'] = page
+
+        if limit:
+            params['limit'] = limit
 
         return OpenMensa.__make_json_request(
             'canteens/{}/days'.format(canteen_id),
@@ -99,16 +132,27 @@ class OpenMensa:
         )
 
     @staticmethod
-    def meals_per_day(canteen_id, date):
+    def meals_per_day(canteen_id, date, page=None, limit=None):
         """Get all meals
         :param canteen_id: id of canteen to query
         :param date: date to get the meals for, format: YYYY-MM-DD
+        :param page: page index to query
+        :param limit: restrict response to this amount of results
         """
+        params = {}
+
+        if page:
+            params['page'] = page
+
+        if limit:
+            params['limit'] = limit
+
         return OpenMensa.__make_json_request(
             'canteens/{}/days/{}/meals'.format(
                 canteen_id,
                 date
             ),
+            params
         )
 
     @staticmethod
