6b88662abd48085f412bc7f3d0c6950c7fb57314
brennerm <xamrennerb@gmail.com>
2018-02-03 19:35:52 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='openmensa-api',
+    packages=['openmensa'],
+    version='1.0',
+    description='Library to talk to OpenMensa API v2',
+    author='Max Brenner',
+    author_email='xamrennerb@gmail.com',
+    url='https://github.com/brennerm/openmensa-api',
+    download_url='https://github.com/brennerm/openmensa-api/archive/1.0.tar.gz',
+    keywords=['openmensa', 'api']
+)
