1d98651d71d203f80b5ed33dfd2707c58088a233
brennerm <xamrennerb@gmail.com>
2018-02-04 20:14:33 +0100

    set version to 1.1

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name='openmensa-api',
     packages=['openmensa'],
-    version='1.0',
+    version='1.1',
     description='Library to talk to OpenMensa API v2',
     author='Max Brenner',
     author_email='xamrennerb@gmail.com',
     url='https://github.com/brennerm/openmensa-api',
-    download_url='https://github.com/brennerm/openmensa-api/archive/1.0.tar.gz',
+    download_url='https://github.com/brennerm/openmensa-api/archive/1.1.tar.gz',
     keywords=['openmensa', 'api']
 )
